8a7c48335b0911c6bfe880e0fd0df9ae78935e47
Lucas Marques <lucasmarquesds@gmail.com>
2023-11-18 10:32:33 -0300

    feat: progress bar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import HabitCard from "@/components/habit-card";
 import Image from "next/image";
 
 export default function Home() {
-  return <HabitCard name="Beber água" unit="copos" quantity={5} actual={3} />;
+  return <HabitCard name="Beber água" unit="copos" quantity={5} actual={0} />;
 }
--- src/components/habit-card.tsx
+++ src/components/habit-card.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 interface HabitCardProps {
   name: string;
   unit: string;
@@ -5,21 +7,83 @@ interface HabitCardProps {
   actual: number;
 }
 
-export default function HabitCard(card: HabitCardProps) {
+interface CircleProps {
+  percent: number;
+}
+
+const Circle: React.FC<CircleProps> = ({ percent }) => {
+  const radius = 50;
+  const circumference = 2 * Math.PI * radius;
+
   return (
-    <div className="flex flex-col items-center mx-auto p-6 max-w-sm rounded-xl shadow-lg bg-orange-100 space-y-3">
-      <div>{card.name}</div>
-      <div className="flex flex-col items-center font-bold">
-        <span>
-          {card.actual} / {card.quantity}
-        </span>
-        <span>{card.unit}</span>
+    <circle
+      className="text-blue-600"
+      strokeWidth="10"
+      strokeDasharray={circumference}
+      strokeDashoffset={circumference - (percent / 100) * circumference}
+      strokeLinecap="round"
+      stroke="currentColor"
+      fill="transparent"
+      r={radius}
+      cx="60"
+      cy="60"
+    />
+  );
+};
+
+export default function HabitCard(props: HabitCardProps) {
+  const [actual, setActual] = useState(props.actual);
+
+  const increaseActual = () => {
+    if (actual < props.quantity) {
+      setActual(actual + 1);
+    }
+  };
+
+  const decreaseActual = () => {
+    if (actual > 0) {
+      setActual(actual - 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center mx-auto my-5 p-6 max-w-sm rounded-xl shadow-lg bg-orange-100 space-y-3">
+      <div>{props.name}</div>
+      <div className="flex items-center justify-center -m-6 overflow-hidden rounded-full">
+        <svg
+          className="w-32 h-32 transform translate-x-1 translate-y-1"
+          x-cloak
+          aria-hidden="true"
+        >
+          <circle
+            className="text-gray-300"
+            strokeWidth="10"
+            stroke="currentColor"
+            fill="transparent"
+            r="50"
+            cx="60"
+            cy="60"
+          />
+          <Circle percent={(actual / props.quantity) * 100} />
+        </svg>
+        <div className="absolute flex flex-col items-center font-bold">
+          <span>
+            {actual} / {props.quantity}
+          </span>
+          <span>{props.unit}</span>
+        </div>
       </div>
       <div className="flex flex-row space-x-3">
-        <button className="bg-gray-700 hover:bg-gray-600 text-white mx-0 border border-gray-600 rounded-sm shadow-sm flex items-center justify-center w-5 h-5">
+        <button
+          className="bg-gray-700 hover:bg-gray-600 text-white mx-0 border border-gray-600 rounded-sm shadow-sm flex items-center justify-center w-5 h-5"
+          onClick={increaseActual}
+        >
           +
         </button>
-        <button className="bg-gray-700 hover:bg-gray-600 text-white mx-0 border border-gray-600 rounded-sm shadow-sm flex items-center justify-center w-5 h-5">
+        <button
+          className="bg-gray-700 hover:bg-gray-600 text-white mx-0 border border-gray-600 rounded-sm shadow-sm flex items-center justify-center w-5 h-5"
+          onClick={decreaseActual}
+        >
           -
         </button>
       </div>
